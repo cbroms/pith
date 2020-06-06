64880855f9e310289c8cb33f885d927f541a4a21
Sydney Zheng <tlz9472@gmail.com>
2020-06-05 23:07:25 -0400

    get_ funcs added for testing

--- tests/test_app.js
+++ tests/test_app.js
@@ -2,6 +2,12 @@
 // python3 app.py in main
 var socket = io('http://localhost:5000');
 socket.on('~connect', function(data){console.log(data)});
+
 socket.emit('get_users', {}, function(data){console.log(data)})
 socket.on('~get_users', function(data){console.log(data)});
+socket.emit('get_posts', {}, function(data){console.log(data)})
+socket.on('~get_posts', function(data){console.log(data)});
+socket.emit('get_blocks', {}, function(data){console.log(data)})
+socket.on('~get_blocks', function(data){console.log(data)});
+
 socket.on('~disconnect', function(){});
