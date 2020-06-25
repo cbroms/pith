37ac4070c6af16c8238349d35f9bc08e23f49d51
Sydney Zheng <tlz9472@gmail.com>
2020-06-25 17:22:53 -0400

    user cleaner dict

--- models/user.py
+++ models/user.py
@@ -1,21 +1,12 @@
 class User():
     def __init__(self, _id, **entries):
         if "_id" in entries:
-            self._id = entries["_id"]
+            self.__dict__ = entries
         else:
             self._id = _id
-        if "library" in entries:
-            self.library = entries["library"]
-        else:
             self.library = {}
             self.library["discussions"] = [] # saved discussions
             self.library["posts"] = [] # saved posts
             self.library["blocks"] = [] # saved blocks
-        if "history" in entries:
-            self.history = entries["history"]
-        else:
             self.history = [] # list of posts
-        if "discussions" in entries:
-            self.discussions = entries["discussions"]
-        else:
             self.discussions = [] # list of discussions user is in
