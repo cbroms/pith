e5f9a3035292b1e70e0f11d7699e802c57737c54
Sydney Zheng <tlz9472@gmail.com>
2020-06-25 17:23:49 -0400

    discussion cleaner dict

--- models/discussion.py
+++ models/discussion.py
@@ -6,22 +6,10 @@ import utils
 class Discussion():
     def __init__(self, **entries):
         if "_id" in entries:
-            self._id = entries["_id"]
+            self.__dict__ = entries
         else:
             self._id = uuid.uuid4().hex
-        if "tags" in entries:
-            self.tags = entries["tags"]
-        else:
             self.tags = []
-        if "users" in entries:
-            self.users = entries["users"]
-        else:
             self.users = []
-        if "history" in entries:
-            self.history = entries["history"]
-        else:
             self.history = []
-        if "history_blocks" in entries:
-            self.history_blocks = entries["history_blocks"]
-        else:
             self.history_blocks = []
