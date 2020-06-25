d9336d02c1a2229a55d0d3334041c2bd723db55b
Sydney Zheng <tlz9472@gmail.com>
2020-06-25 17:21:47 -0400

    post cleaner dict

--- models/post.py
+++ models/post.py
@@ -9,31 +9,13 @@ date_time_fmt = "%Y-%m-%dT%H:%M:%SZ"
 class Post():
     def __init__(self, user, discussion, **entries):
         if "_id" in entries:
-            self._id = entries["_id"]
+            self.__dict__ = entries
         else:
             self._id = uuid.uuid4().hex
-        if "user" in entries:
-            self.user = entries["user"]
-        else:
             self.user = user
-        if "discussion" in entries:
-            self.discussion = entries["discussion"]
-        else:
             self.discussion = discussion
-        if "blocks" in entries:
-            self.blocks = entries["blocks"]
-        else:
-            self.blocks = None # to be set
-        if "freq_dict" in entries:
-            self.freq_dict = defaultdict(lambda:0, entries["freq_dict"])
-        else:
-            self.freq_dict = None # to be set 
-        if "tags" in entries:
-            self.tags = entries["tags"]
-        else:
+            self.blocks = None
+            self.freq_dict = None
             self.tags = []
-        if "created_at" in entries:
-            self.created_at = entries["created_at"]
-        else:
             self.created_at = datetime.utcnow().strftime(date_time_fmt)
             # convert back: datetime.strptime(self.created_at, date_time_fmt)
