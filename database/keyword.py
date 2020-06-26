7af54da60d18efb20748288ecae875bc95b1f359
Sydney Zheng <tlz9472@gmail.com>
2020-06-26 17:10:03 -0400

    turned database files into classes

--- database/keyword.py
+++ database/keyword.py
@@ -7,41 +7,38 @@ from constants import (
 )
 
 
-def get_keyword_blocks(keyword):
-    keyword_data = keywords.find_one({ "_id" : keyword })
-    if not keyword_data: return {}
-    blocks = keyword_data["blocks"]
-    return blocks
+class KeywordDatabase:
 
+    def get_keyword_blocks(self, keyword):
+        keyword_data = keywords.find_one({ "_id" : keyword })
+        if not keyword_data: return {}
+        blocks = keyword_data["blocks"]
+        return blocks
 
-def add_keyword_block(keyword, block_id, freq):
-    keywords.update_one(
-        {"_id" : keyword},
-        {"$set": {"blocks.{}".format(block_id) : {"freq" : freq}}},
-        upsert=True
-    )
+    def add_keyword_block(self, keyword, block_id, freq):
+        keywords.update_one(
+            {"_id" : keyword},
+            {"$set": {"blocks.{}".format(block_id) : {"freq" : freq}}},
+            upsert=True
+        )
 
+    def index_block(self, block_id, freq_dict):
+        for k,f in freq_dict.items():
+            add_keyword_block(k, block_id, f)
 
-def index_block(block_id, freq_dict):
-    for k,f in freq_dict.items():
-        add_keyword_block(k, block_id, f)
+    def get_keyword_posts(self, keyword):
+        keyword_data = keywords.find_one({ "_id" : keyword })
+        if not keyword_data: return {}
+        posts = keyword_data["posts"]
+        return posts
 
+    def add_keyword_post(self, keyword, post_id, freq):
+        keywords.update_one(
+            {"_id" : keyword},
+            {"$set": {"posts.{}".format(post_id) : {"freq" : freq}}},
+            upsert=True
+        )
 
-def get_keyword_posts(keyword):
-    keyword_data = keywords.find_one({ "_id" : keyword })
-    if not keyword_data: return {}
-    posts = keyword_data["posts"]
-    return posts
-
-
-def add_keyword_post(keyword, post_id, freq):
-    keywords.update_one(
-        {"_id" : keyword},
-        {"$set": {"posts.{}".format(post_id) : {"freq" : freq}}},
-        upsert=True
-    )
-
-
-def index_post(post_id, freq_dict):
-    for k,f in freq_dict.items():
-        add_keyword_post(k, post_id, f)
+    def index_post(self, post_id, freq_dict):
+        for k,f in freq_dict.items():
+            add_keyword_post(k, post_id, f)
--- database/post.py
+++ database/post.py
@@ -13,43 +13,38 @@ from user import (
 )
 
 
-def get_posts():
-    post_cursor = posts.find()
-    post_list = []
-    for u in post_cursor:
-        post_list.append(u)
-    return post_list
+class PostDatabase:
 
+    def get_posts(self):
+        post_cursor = posts.find()
+        post_list = []
+        for u in post_cursor:
+            post_list.append(u)
+        return post_list
 
-def get_discussion_posts(discussion_id):
-    discussion_data = get_discussion(discussion_id)
-    history = discussion_data["history"]
-    return history
+    def get_discussion_posts(self, discussion_id):
+        discussion_data = get_discussion(discussion_id)
+        history = discussion_data["history"]
+        return history
 
+    def get_user_saved_posts(self, user_id):
+        user_data = get_user(user_id)
+        saved = user_data["library"]["posts"]
+        return saved
 
-def get_user_saved_posts(user_id):
-    user_data = get_user(user_id)
-    saved = user_data["library"]["posts"]
-    return saved
+    def get_post(self, post_id):
+        post_data = posts.find_one({ "_id" : post_id })
+        return post_data
 
+    def insert_post(self, post_obj):
+        post_data = post_obj.__dict__
+        posts.insert_one(post_data)
 
-def get_post(post_id):
-    post_data = posts.find_one({ "_id" : post_id })
-    return post_data
+    def save_post(self, post_id, user_id):
+        users.update_one({"_id" : user_id}, {"$push" : {"library.posts" : post_id}})
 
+    def post_add_tag(self, post_id, tag):
+        posts.update_one({"_id" : post_id}, {"$push": {"tags" : tag}})
 
-def insert_post(post_obj):
-    post_data = post_obj.__dict__
-    posts.insert_one(post_data)
-
-
-def save_post(post_id, user_id):
-    users.update_one({"_id" : user_id}, {"$push" : {"library.posts" : post_id}})
-
-
-def post_add_tag(post_id, tag):
-    posts.update_one({"_id" : post_id}, {"$push": {"tags" : tag}})
-
-
-def post_remove_tag(post_id, tag):
-    posts.update_one({"_id" : post_id}, {"$pull": {"tags" : tag}})
+    def post_remove_tag(self, post_id, tag):
+        posts.update_one({"_id" : post_id}, {"$pull": {"tags" : tag}})
