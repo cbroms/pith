a4360b17b383a0e312a14d274f95080e9765f381
Christian Broms <cb@christianbroms.com>
2020-08-31 21:24:49 -0400

    add env defaults

--- constants.py
+++ constants.py
@@ -11,11 +11,11 @@ load_dotenv()
 
 # connect to the redis server instance. When running locally, use 127.0.0.1, or
 # use the container name if using docker within a docker network
-SOCKET_REDIS = "redis://{}:{}/0".format(os.getenv("REDIS_IP"), os.getenv("REDIS_PORT"))
+SOCKET_REDIS = "redis://{}:{}/0".format(os.getenv("REDIS_IP", "127.0.0.1"), os.getenv("REDIS_PORT", 6379))
 
 # the job queue's redis configuration. Could use a different redis server
 # instance than the socket message queue, but for now we can keep them the same.
-ARQ_REDIS = RedisSettings(host=os.getenv("REDIS_IP"), port=int(os.getenv("REDIS_PORT")))
+ARQ_REDIS = RedisSettings(host=os.getenv("REDIS_IP", "127.0.0.1"), port=int(os.getenv("REDIS_PORT", 6379)))
 
 # the maximum number to times we should try to rerun a task in the queue if it fails
 MAX_QUEUED_JOB_RETRIES = 5
@@ -25,7 +25,7 @@ MAX_JOBS = 10
 
 # using ISO so this can be easily parsed in js with Date()
 DATE_TIME_FMT = "%Y-%m-%dT%H:%M:%SZ"
-MONGO_CONN = os.getenv("MONGO_CONN")
+MONGO_CONN = os.getenv("MONGO_CONN", "mongodb://localhost:27017")
 MONGODB_NAME = "db"
 
 # compiled searcher for transclusion header
