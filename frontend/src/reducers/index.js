6e2d7ed451f9dec3404978cf3fd03992d76e28bc
= <cb@christianbroms.com>
2020-12-22 10:29:33 -0800

    add sapper project

--- frontend/src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-
-import board from "./boardReducer";
-import discussion from "./discussionReducer";
-
-export default combineReducers({
-	board: board,
-	discussion: discussion,
-});
