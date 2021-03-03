122284e9c484d6b2f216d9842af98cdfb82271d4
Christian Broms <cb@christianbroms.com>
2021-03-03 17:33:19 -0500

    (resolve #40) add 'copied' after successfuly copying an id to the clipboard

--- frontend/src/components/buttons/CopyContent.svelte
+++ frontend/src/components/buttons/CopyContent.svelte
@@ -1,16 +1,34 @@
 <script>
   export let id;
 
+  let displayMessage = false;
+
   const onIdClick = async () => {
     try {
-      await navigator.clipboard.writeText(id);
-      alert(
-        `Copied unit ID!\n\nReference it in a message using double brackets: [[ ${id} ]]`
-      );
+      await navigator.clipboard.writeText(`[[${id}]]`);
+      displayMessage = true;
+      setTimeout(() => {
+        displayMessage = false;
+      }, 3000);
     } catch (err) {
       console.error("Failed to copy: ", err);
+      console.error(
+        "See https://github.com/rainflame/pith/issues/43#issuecomment-790097727 for more information."
+      );
     }
   };
 </script>
 
-<button on:click={onIdClick}>#</button>
+{#if displayMessage}
+  <div class="copied-message">Copied!</div>
+{:else}
+  <button on:click={onIdClick}>#</button>
+{/if}
+
+<style>
+  .copied-message {
+    display: inline-block;
+    font-size: 12px;
+    padding: 0 10px;
+  }
+</style>
